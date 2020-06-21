a1d732c15acba3f5a361d539f13289be703b4fb6
Alexander <alexanderkilroy@outlook.com>
2020-06-21 21:40:31 +0100

    Accept TCP connection Accepts general TCP connection and parses the binary.
    Further breaks the response down into subarrays, primarily to recognise the headers

--- src/Server.ts
+++ src/Server.ts
@@ -1,1 +1,34 @@
-console.log("running it");
+import net, { Server, Socket } from "net";
+
+class ServerWrapper {
+    private _server: Server;
+
+    constructor() {
+        this._server = new Server();
+        this._server.on("connection", (socket: Socket) => {
+            socket.on("data", (data) => {
+                // Grab request and split headers down into an array
+                let decoded = data.toString().split("\n");
+                // Parse into nested array for object prep
+                let headersPreParse = decoded.map(str => {
+                    return str.replace(/(\r)|(, br)/g, "").split(":");
+                });
+                // TODO: Handle other HTTP versions
+                let methodAndEndpoint = headersPreParse[0][0]
+                    .replace(/( HTTP\/1.1)/, "")
+                    .split(" ");
+                console.log(headersPreParse);
+                console.log(methodAndEndpoint);
+
+            });
+        });
+    }
+
+    public start(): void {
+        this._server.listen(3001, () => {
+            console.log("Server listening on 3001");
+        });
+    }
+}
+
+new ServerWrapper().start();
