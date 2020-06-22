7e09356688a9d9858c5479275022a6eb901e2796
Alexander <alexanderkilroy@outlook.com>
2020-06-23 00:47:38 +0100

    Comments for ideas
    Veeeeery tiny commit, just some ideas I'd like to make sure I don't forget.

--- src/Server.ts
+++ src/Server.ts
@@ -12,28 +12,29 @@ class ServerWrapper {
         this._server = new Server();
         this._server.on("connection", (socket: Socket) => {
             socket.on("data", (data: Buffer) => {
+                let testing;
                 // Grab request and split headers down into an array
                 let decoded = data
                     .toString()
                     .split("\n")
-                    .map((str) => str.replace(/(\r)|(, br)/g, "").split(":"));
+                    .map((str: string) => str.replace(/(\r)|(, br)/g, "").split(":"));
                 // The content is always followed by [""], so we know where to split.
                 // We also want this content off of the decoded headers
-                const payloadIndex = decoded.findIndex((value) => value[0] === "");
+                const payloadIndex = decoded.findIndex((value: string[]) => value[0] === "");
                 const payload = decoded.splice(payloadIndex, decoded.length);
                 // Grab the method and uri path and format it
                 decoded = decoded.concat(
                     decoded
                         .shift()
-                        ?.reduce((prev, curr) => curr)
+                        ?.reduce((prev: string, curr: string) => curr)
                         .replace(/( HTTP\/1.1)/, "")
                         .split(" ")
-                        .map((val, i) => (i === 0 ? ["Method", val] : ["URI-Path", val])) as string[][]
+                        .map((val: string, i: number) => (i === 0 ? ["Method", val] : ["URI-Path", val])) as string[][]
                 );
                 // Turn into objects
                 const decodedObjArr = decoded
-                    .map((arr) => arr.map((str) => str.trim()))
-                    .map((arr) => {
+                    .map((arr: string[]) => arr.map((str: string) => str.trim()))
+                    .map((arr: string[]) => {
                         if (arr[0] === "Host") {
                             return ["Host", `${arr[1]}:${arr[2]}`];
                         } else {
@@ -46,6 +47,8 @@ class ServerWrapper {
                 // Finalize it into a single object
                 let final = {};
                 Object.assign(final, ...decodedObjArr);
+                console.log(final);
+                console.log(payload);
             });
         });
     }
@@ -58,3 +61,25 @@ class ServerWrapper {
 }
 
 new ServerWrapper().start();
+
+/**
+ * Ideas for creating instances:
+ * 
+ * let server = new ServerWrapper(port, host?, options?);
+ * 
+ * server.addRoute({
+ *  route: string;
+ *  method: string;
+ *  controller: controllerReference;
+ * });
+ * 
+ * or:
+ * 
+ * import { Router } from "blahblahblah";
+ * Router.get("endpoint/here", controllerReference);
+ * Router.etc...
+ * 
+ * class Controller {
+ * ???????????????????????????????????????????????????
+ * }
+ */
