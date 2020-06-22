2eb600689f5f03f76fe162b69b0a6cffe1e64ca1
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2020-06-22 15:34:45 +0100

    Pass data into single [][]

--- src/Server.ts
+++ src/Server.ts
@@ -1,5 +1,11 @@
 import net, { Server, Socket } from "net";
+import { cursorTo } from "readline";
 
+/**
+ * Supports only HTTP 1.1
+ *
+ * @author ale8k
+ */
 class ServerWrapper {
     private _server: Server;
 
@@ -8,26 +14,26 @@ class ServerWrapper {
         this._server.on("connection", (socket: Socket) => {
             socket.on("data", (data) => {
                 // Grab request and split headers down into an array
-                let decoded = data.toString().split("\n");
-                // Parse into nested array for object prep
-                let headersPreParse = decoded.map(str => {
-                    return str.replace(/(\r)|(, br)/g, "").split(":");
-                });
-                // TODO: Handle other HTTP versions
-                let methodAndEndpoint = headersPreParse[0][0]
-                    .replace(/( HTTP\/1.1)/, "")
-                    .split(" ");
-
-                // Find content type
-                // Find content length
-                // Parse content (body) out of headersPreParse
-                // Complete header object
-                const contentType = headersPreParse.filter(header => {
-                    return header[0] === "Content-Type";
-                })[0][1].trim();
-                console.log(contentType);
-                
-
+                let decoded = data
+                    .toString()
+                    .split("\n")
+                    .map((str) => str.replace(/(\r)|(, br)/g, "").split(":"));
+                // The content is always followed by [""], so we know where to split.
+                // We also want this content off of the decoded headers
+                const payloadIndex = decoded.findIndex((value) => value[0] === "");
+                const payload = decoded.splice(payloadIndex, decoded.length);
+                // Grab the method and uri path and format it
+                decoded = decoded.concat(
+                    decoded
+                        .shift()
+                        ?.reduce((prev, curr) => curr)
+                        .replace(/( HTTP\/1.1)/, "")
+                        .split(" ")
+                        .map((val, i) => (i === 0 ? ["Method", val] : ["URI-Path", val])) as string[][]
+                );
+                console.log(decoded, "final");
+                // Now we need to reduce any nested array with more than 2 elements,
+                // as elements > 1 are a single value for this header
             });
         });
     }
